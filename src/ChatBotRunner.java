c557eb255a87a31cf06963a995cbf7718459122b
student <bonniec15@nycstudents.net>
2022-09-28 10:56:40 -0400

    Question 17

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,26 @@
+public class ChatBotRunner {
+    public static void main (String [] args) {
+
+        //use of greeting
+        ChatBot chatBot1 = new ChatBot("Debbie", 2);
+        chatBot1.greeting("Bonnie");
+        System.out.println();
+
+        //use of weather
+        System.out.println("I'm good! How's the weather today?");
+        System.out.println();
+        chatBot1.weather();
+        System.out.println();
+
+        //use of favoriteNumbers
+
+
+
+
+
+
+
+
+
+    }
+}
